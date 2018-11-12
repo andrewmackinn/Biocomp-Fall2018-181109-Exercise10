86bb3b3ddd958285322fd5f263108b1fea864899
Andrew MacKinnon <amackinn@nd.edu>
2018-11-12 17:15:11 -0500

    first model

--- Script10.R
+++ Script10.R
@@ -1,9 +1,20 @@
 dat = read.table("data.txt", sep = ",", header = TRUE, stringsAsFactors = FALSE)
 nnlike= function(p,x,y){
-  
+  a =p[1]
+  b=p[2]
+  c=p[3]
+  sigma = exp(p[4])
+  expected = a +b*x +c*x^2
+  nll = sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
 }
+initalGuess = c(1,1,1,1)
+fit = optim(par = initialGuess, fn =nllike, x = dat$x, y = dat$y)
+print(fit)
 
-expected = a +b*x +c*x^2
-r <1
-k = 1/alpha 
-alpha <.1
+
+            
+
+#expected = a +b*x +c*x^2
+#r <1
+#k = 1/alpha 
+#alpha <.1
